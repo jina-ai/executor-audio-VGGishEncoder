dc6cad503b1bef9a6be5dec799fc40e8721fb05b
jakob1111996 <jakob.kruse@jina.ai>
2021-06-30 11:36:19 +0200

    chore: bump version, improve model download

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -3,14 +3,14 @@ MODEL_DIR=models
 DATA_DIR=data
 REPO_DIR=audioset_tagging_cnn
 
-if [ -d "$MODEL_DIR" ]; then
-  echo $MODEL_DIR ' exists. Please delete it to continue'
-  exit 1
-fi
-
-echo "Downloading model"
 mkdir -p ${MODEL_DIR}
-echo "------ Download Vggish model ------"
-curl https://storage.googleapis.com/audioset/vggish_model.ckpt --output ${MODEL_DIR}/vggish_model.ckpt
-echo "------ Download PCA model ------"
-curl https://storage.googleapis.com/audioset/vggish_pca_params.npz --output ${MODEL_DIR}/vggish_pca_params.npz
+
+if [ ! -f "${MODEL_DIR}/vggish_model.ckpt" ]; then
+  echo "Downloading model"
+  echo "------ Download Vggish model ------"
+  curl https://storage.googleapis.com/audioset/vggish_model.ckpt --output ${MODEL_DIR}/vggish_model.ckpt
+  echo "------ Download PCA model ------"
+  curl https://storage.googleapis.com/audioset/vggish_pca_params.npz --output ${MODEL_DIR}/vggish_pca_params.npz
+else
+  echo "Model already exists! Skipping."
+fi
